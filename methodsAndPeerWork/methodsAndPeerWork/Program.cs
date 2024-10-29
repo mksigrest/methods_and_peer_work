021252c736402ce7f3f5d5b07d38a79713b8a402
mksigrest <mksigrest@gmail.com>
2024-10-29 08:15:14 -0400

    initial Methods and Peer Work Push
    start push for methods and peer work

---
+++ methodsAndPeerWork/methodsAndPeerWork/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace methodsAndPeerWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
