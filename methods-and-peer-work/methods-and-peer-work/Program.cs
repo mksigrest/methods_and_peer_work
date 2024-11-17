a3d6301bbe305e0b2259c4263fd72a195157937e
mksigrest <mksigrest@gmail.com>
2024-11-17 17:01:21 -0500

    push 1
    initial program uploaded to Github

---
+++ methods-and-peer-work/methods-and-peer-work/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace methods_and_peer_work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
