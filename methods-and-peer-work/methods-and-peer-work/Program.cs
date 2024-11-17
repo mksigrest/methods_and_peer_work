bffe3997b3ff701469f94cd16f98d695ae8f2981
mksigrest <mksigrest@gmail.com>
2024-11-17 17:21:57 -0500

    push 2
    completed code minus bug checks

--- methods-and-peer-work/methods-and-peer-work/Program.cs
+++ methods-and-peer-work/methods-and-peer-work/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //declaration
+            int maxFavNum;
+            int partnerFavNum;
+            int addValue;
+            int subValue;
+
+            //initialize
+            maxFavNum = 10;
+            partnerFavNum = 8;
+
+            static int Subtract(int int3, int int4)
+            {
+                int value2 = int3 - int4;
+                return value2;
+            }
+
+            //start of actual program
+            Console.WriteLine("Max's favorite number is: " + maxFavNum);
+            Console.WriteLine("Partners favorite number is: " + partnerFavNum);
+
+            addValue = Add(maxFavNum, partnerFavNum);
+            Console.WriteLine("When we add out favorite numbers together we get: " + addValue);
+
+            subValue = Subtract(maxFavNum, partnerFavNum);
+            Console.WriteLine("When we subtract our favorite numbers together we get: " + subValue);
+        }
+
+        static int Add(int int1, int int2)
+        {
+            int value = int1 + int2;
+            return value;
         }
     }
 }
